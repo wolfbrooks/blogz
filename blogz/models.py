2a47303f6580311f6c4ccb7914cdc16fe5dae97b
wolfbrooks <briannagreene7@gmail.com>
2017-04-21 09:51:46 -0500

    Add files via upload

---
+++ blogz/models.py
@@ -0,0 +1,13 @@
+from google.appengine.ext import db
+
+class User(db.Model):
+    username = db.StringProperty(required = True)
+    pw_hash = db.StringProperty(required = True)
+    email = db.StringProperty()
+
+class Post(db.Model):
+    author = db.ReferenceProperty(User, required = True)
+    title = db.StringProperty(required = True)
+    body = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+    # TODO - DONE we need an author field here; it should be required
